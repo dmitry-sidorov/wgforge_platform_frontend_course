965a406636b66f052065a0e43ea2dfad368d0f0e
Dmitry <jimmaggot@mail.ru>
2019-04-15 13:01:29 +0300

    add cloneDeep solution

--- 03_iterators/practice/exercises/cloneDeep.js
+++ 03_iterators/practice/exercises/cloneDeep.js
@@ -1,6 +1,24 @@
 /**
  * Реализовать функцию cloneDeep которая копирует объекты по значению с любой глубиной вложенности
  */
-export default function cloneDeep() {
-  // ¯\_(ツ)_/¯
+export default function cloneDeep(sourceObject) {
+  // lifehack =)))
+  // return JSON.parse(JSON.stringify(sourceObject));
+
+  const isObject = obj => obj.__proto__.constructor.name.toLowerCase() === 'object';
+  const isArray = obj => obj.__proto__.constructor.name.toLowerCase() === 'array';
+  let clonedObject;
+  if (isObject(sourceObject)) {
+    clonedObject = {};
+  } else {
+    if (isArray(sourceObject)) {
+      clonedObject = [];
+    } else {
+      return sourceObject;
+    }
+  }
+  for (let i in sourceObject) {
+    clonedObject[i] = cloneDeep(sourceObject[i]);
+  }
+  return clonedObject;
 }
