e159a41725fd98beed0d50845c99a17f44d7b40e
Dmitry <jimmaggot@mail.ru>
2019-04-23 14:49:32 +0300

    add turret and tank  movement

--- 05_markup/project/index.js
+++ 05_markup/project/index.js
@@ -1,22 +1,33 @@
+const body = document.querySelector('body');
 const sections = document.querySelectorAll('section');
-const tank = document.getElementById('tank');
 const turret = document.getElementById('turret');
+const movingTank = document.getElementById('movingTank');
+
+body.addEventListener('click', event => {
+  body.classList.add('toggle');
+});
+
 for (let section of sections) {
   section.addEventListener('mouseenter', event => {
-    const turretDirection = event.target.dataset.turretDirection;
-
-    turret.classList.add(`tank_turret__${turretDirection}`);
-    console.log(turret.classList);
+    const directionSelector = event.target.dataset.turretDirection;
+    const lastIndex = turret.classList.length - 1;
+    const previousDirection = turret.classList[lastIndex];
+    const currentDirection = `tank_turret__${directionSelector}`;
+    if (previousDirection !== currentDirection) {
+      if (turret.classList.length > 1) {
+        turret.classList.remove(previousDirection);
+      }
+      turret.classList.add(currentDirection);
+    }
   });
 }
 
-const tank_2 = document.getElementById('tank2');
-
-let lastPos = 0;
 let lastScrollTop = 0;
-section[0].addEventListener('scroll', event => {
-  let curretTop = event.targer.scrollTop;
+sections[0].addEventListener('scroll', event => {
+  let currentTop = event.target.scrollTop;
   let diff = currentTop - lastScrollTop;
-
-  tank_2.style.transform = translate();
+  let diffCorrected = currentTop - lastScrollTop - 1020;
+  let duration = Math.round(diff / 50) / 10;
+  movingTank.style.transform = `translateX(${diffCorrected}px)`;
+  movingTank.style.transition = `transform ${duration}s ease`;
 });
